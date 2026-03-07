e313302be35ddf4e0446acd595a75aca3e3c9e02
waliullah8328 <wali15-14044@diu.edu.bd>
2026-03-07 23:01:39 +0600

    settings screen added

--- lib/feature/home_section/calculator_section/setting_section/settings/screen/setting_screen.dart
+++ lib/feature/home_section/calculator_section/setting_section/settings/screen/setting_screen.dart
@@ -8,6 +8,7 @@ import 'package:swim_metrics/core/utils/constants/app_sizer.dart';
 import 'package:swim_metrics/core/utils/constants/image_path.dart';
 
 import '../../../../../../core/common/widgets/custom_text.dart';
+import '../../../../../../core/common/widgets/new_custon_widgets/custom_switch_widget.dart';
 import '../../../../../../core/utils/constants/icon_path.dart';
 import '../riverpod/setting_controller.dart';
 
@@ -16,11 +17,25 @@ class SettingsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+
+    double getAdjustedFontSize(double baseSize, FontSizeOption option) {
+      switch (option) {
+        case FontSizeOption.small:
+          return baseSize - 2;
+        case FontSizeOption.medium:
+          return baseSize;
+        case FontSizeOption.big:
+          return baseSize + 2;
+      }
+    }
     final settings = ref.watch(settingsProvider);
+    final fontOption = ref.watch(settingsProvider).fontSize;
+
+
 
     return Scaffold(
       appBar: AppBar(
-        title: CustomText(text: "Settings",fontSize: 24.sp,fontWeight: FontWeight.w600,),
+        title: CustomText(text: "Settings",fontSize: getAdjustedFontSize(24, fontOption).sp,fontWeight: FontWeight.w600,),
         centerTitle: true,
         leading: GestureDetector(
           onTap: (){
@@ -69,13 +84,13 @@ class SettingsScreen extends ConsumerWidget {
                           text:
                           "Mr. Sahil Khan",
                           fontWeight: FontWeight.w600,
-                          fontSize: 19.sp,
+                          fontSize: getAdjustedFontSize(19, fontOption).sp,
                         ),
                         CustomText(
                           text:
                           "Premium user",
                           fontWeight: FontWeight.w400,
-                          fontSize: 15.sp,
+                          fontSize: getAdjustedFontSize(15, fontOption).sp,
                           color: AppColors.primary,
                         ),
                       ],
@@ -89,7 +104,7 @@ class SettingsScreen extends ConsumerWidget {
                       color: AppColors.textGrey,
                       child: Padding(
                       padding: const EdgeInsets.all(14.0),
-                      child: Center(child: CustomText(text: "Edit Profile",fontSize: 14.sp,fontWeight: FontWeight.w400,)),
+                      child: Center(child: CustomText(text: "Edit Profile",fontSize: getAdjustedFontSize(12, fontOption).sp,fontWeight: FontWeight.w400,)),
                     ),),
                   ),
                 ],
@@ -102,42 +117,218 @@ class SettingsScreen extends ConsumerWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const SizedBox(height: 20),
+                 SizedBox(height: 20.h),
 
                 /// APPEARANCE
-                const Text("APPEARANCE"),
+                CustomText(text: "APPEARANCE",fontSize: getAdjustedFontSize(18, fontOption).sp,fontWeight: FontWeight.w600,),
+                SizedBox(height: 10.h,),
 
-                SwitchListTile(
-                  title: const Text("Dark Mode"),
-                  value: settings.darkMode,
-                  onChanged: (v) =>
-                      ref.read(settingsProvider.notifier).toggleDarkMode(v),
+                Card(
+                  color: Color(0xffFFFFFF),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                SvgPicture.asset(IconPath.darkModeLightIcon ),
+                                SizedBox(width: 10.w,),
+                                CustomText(text: "Dark Mode",fontSize: getAdjustedFontSize(16, fontOption).sp,fontWeight: FontWeight.w500,),
+                              ],
+                            ),
+                            Consumer(builder: (context, ref, child) {
+                              final isRemember = ref.watch(settingsProvider.select((s)=>s.darkMode));
+                              return CustomSwitchWidget(
+                                value: isRemember,
+                                onChanged: (value) {
+                                  ref.read(settingsProvider.notifier)
+                                      .toggleDarkMode(value,ref );
+
+
+                                },
+                              );
+                            },),
+                          ],
+                        ),
+                        SizedBox(height: 16.h,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                SvgPicture.asset(IconPath.textSizeIcon),
+                                SizedBox(width: 10.w,),
+                                CustomText(text: "Text Size",fontSize: getAdjustedFontSize(16, fontOption).sp,fontWeight: FontWeight.w500,),
+                              ],
+                            ),
+                            Consumer(
+                              builder: (context, ref, child) {
+                                final fontSize =
+                                ref.watch(settingsProvider.select((s) => s.fontSize));
+
+                                return SizedBox(
+                                  width: 100.w,
+                                  height: 40.h,
+                                  child: Container(
+                                    padding: EdgeInsets.symmetric(horizontal: 10),
+                                    decoration: BoxDecoration(
+                                      color: Color(0xffEAEDF1),
+                                      borderRadius: BorderRadius.circular(45),
+                                      border: Border.all(color: Color(0xffEAEDF1),),
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        Expanded(
+                                          child: DropdownButtonHideUnderline(
+                                            child: DropdownButton<FontSizeOption>(
+                                              value: fontSize,
+                                              isExpanded: true,
+                                              isDense: true,
+                                              icon: const SizedBox(), // removes default icon
+                                              borderRadius: BorderRadius.circular(12),
+                                              items:  [
+                                                DropdownMenuItem(
+                                                  value: FontSizeOption.small,
+                                                  child: CustomText(text: "SMALL",fontSize: 10.sp,),
+                                                ),
+                                                DropdownMenuItem(
+                                                  value: FontSizeOption.medium,
+                                                  child: CustomText(text: "MEDIUM",fontSize: 10.sp,),
+                                                ),
+                                                DropdownMenuItem(
+                                                  value: FontSizeOption.big,
+                                                  child: CustomText(text: "BIG",fontSize: 10.sp,),
+                                                ),
+                                              ],
+                                              onChanged: (value) {
+                                                if (value != null) {
+                                                  ref
+                                                      .read(settingsProvider.notifier)
+                                                      .changeFontSize(value);
+                                                }
+                                              },
+                                            ),
+                                          ),
+                                        ),
+
+                                        /// Your custom icon
+                                        const Icon(
+                                          Icons.keyboard_arrow_down,
+                                          size: 20,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              },
+                            )
+                          ],
+                        ),
+
+                      ],
+                    ),
+                  ),
                 ),
 
-                const SizedBox(height: 20),
+                SizedBox(height: 20.h),
+                CustomText(text: "ACCESSIBILITY",fontSize: getAdjustedFontSize(18, fontOption).sp,fontWeight: FontWeight.w600,),
+                SizedBox(height: 10.h,),
 
-                /// ACCESSIBILITY
-                const Text("ACCESSIBILITY"),
 
-                SwitchListTile(
-                  title: const Text("Stopwatch Sound"),
-                  value: settings.stopwatchSound,
-                  onChanged: (v) =>
-                      ref.read(settingsProvider.notifier).toggleStopwatch(v),
-                ),
 
-                SwitchListTile(
-                  title: const Text("Voice Input"),
-                  value: settings.voiceInput,
-                  onChanged: (v) =>
-                      ref.read(settingsProvider.notifier).toggleVoice(v),
-                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xffFFFFFF),
+                    borderRadius: BorderRadius.circular(12)
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
 
-                SwitchListTile(
-                  title: const Text("Haptic Feedback"),
-                  value: settings.haptic,
-                  onChanged: (v) =>
-                      ref.read(settingsProvider.notifier).toggleHaptic(v),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                SvgPicture.asset(IconPath.stopSoundIcon),
+                                SizedBox(width: 10.w,),
+                                CustomText(text: "Stopwatch Sound",fontSize: getAdjustedFontSize(16, fontOption).sp,fontWeight: FontWeight.w500,),
+                              ],
+                            ),
+                            Consumer(builder: (context, ref, child) {
+                              final isRemember = ref.watch(settingsProvider.select((s)=>s.stopwatchSound));
+                              return CustomSwitchWidget(
+                                value: isRemember,
+                                onChanged: (value) {
+                                  ref.read(settingsProvider.notifier)
+                                      .toggleStopwatch(value);
+
+
+                                },
+                              );
+                            },),
+                          ],
+                        ),
+                        SizedBox(height: 16.h,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                SvgPicture.asset(IconPath.voiceInputIcon),
+                                SizedBox(width: 10.w,),
+                                CustomText(text: "Voice Input",fontSize: getAdjustedFontSize(16, fontOption).sp,fontWeight: FontWeight.w500,),
+                              ],
+                            ),
+                            Consumer(builder: (context, ref, child) {
+                              final isRemember = ref.watch(settingsProvider.select((s)=>s.voiceInput));
+                              return CustomSwitchWidget(
+                                value: isRemember,
+                                onChanged: (value) {
+                                  ref.read(settingsProvider.notifier)
+                                      .toggleVoice(value);
+
+
+                                },
+                              );
+                            },),
+                          ],
+                        ),
+                        SizedBox(height: 16.h,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                SvgPicture.asset(IconPath.hapticIcon),
+                                SizedBox(width: 10.w,),
+                                CustomText(text: "Haptic Feedback",fontSize: getAdjustedFontSize(16, fontOption).sp,fontWeight: FontWeight.w500,),
+                              ],
+                            ),
+                            Consumer(builder: (context, ref, child) {
+                              final isRemember = ref.watch(settingsProvider.select((s)=>s.haptic));
+                              return CustomSwitchWidget(
+                                value: isRemember,
+                                onChanged: (value) {
+                                  ref.read(settingsProvider.notifier)
+                                      .toggleHaptic(value);
+
+
+                                },
+                              );
+                            },),
+                          ],
+                        ),
+
+
+
+                      ],
+                    ),
+                  ),
                 ),
 
                 const SizedBox(height: 30),
