9b8248076edbf78ac26064a45ea3f7cdc1eeee91
waliullah8328 <wali15-14044@diu.edu.bd>
2026-03-13 12:08:23 +0600

    edit profile added

--- lib/feature/home_section/calculator_section/setting_section/edit_profile/riverpod/edit_profile_state.dart
+++ lib/feature/home_section/calculator_section/setting_section/edit_profile/riverpod/edit_profile_state.dart
@@ -1,23 +1,29 @@
+import 'dart:io';
+
 class ProfileState {
   final String name;
   final String email;
   final String phone;
+  final File? profileImage;
 
   ProfileState({
     this.name = "",
     this.email = "",
     this.phone = "",
+    this.profileImage,
   });
 
   ProfileState copyWith({
     String? name,
     String? email,
     String? phone,
+    File? profileImage,
   }) {
     return ProfileState(
       name: name ?? this.name,
       email: email ?? this.email,
       phone: phone ?? this.phone,
+      profileImage: profileImage ?? this.profileImage,
     );
   }
 }
